1e1740c59b5622d3f6abc24a823c35a000a1dfb0
devinchicode <orel214a@gmail.com>
2025-01-16 23:12:03 +0200

    Add files via upload

--- backend_weather.py
+++ backend_weather.py
@@ -6,21 +6,21 @@ class GeoData():
 		self.city = city
 		self.amount_of_days = amount_of_days
 		self.status_code = 0
-		self.parameters = []
+		self.json_response = []
 		self.get_api_values()
 		
-		if "error" in self.parameters:
+		if "error" in self.json_response:
 			return
 
-		self.min_temp = []
-		self.max_temp = []
-		self.avg_temp = []
-		self.humidity = []
-		self.location = []
-		self.icon = []
-		self.current = []
-		self.image = ""
-		self.days = []
+		self.data = {"min_temp":[],
+					 "max_temp":[],
+					 "avg_temp":[],
+					 "humidity":[],
+					 "icon":[],
+					 "location":[],
+					 "current":[],
+					 "days":[],
+					 "image":""}
 
 		self.parse_api_values()
 		self.string_format_weekdays()
@@ -29,30 +29,30 @@ class GeoData():
 
 	def get_api_values(self) -> None:
 		api = Weather_API(self.city, self.amount_of_days)
-		self.parameters = api.get_api_json()
+		self.json_response = api.get_api_json()
 		self.status_code = api.status_code
 
 
 	def parse_api_values(self) -> None:	
 		"""
-		parse the json parameters into lists,
-		each list for specific parameters,
+		parse the json json_response into lists,
+		each list for specific json_response,
 		index in each list order by days,
 		i.e every index [0] in whole lists will be current day,
 		every index [1] in whole lists will be tommorow, etc...
 		"""
-		self.current.append(self.parameters["current"]["temp_c"])
-		self.current.append(self.parameters["forecast"]["forecastday"][0]["day"]["condition"]["text"])
-		self.location.append(self.parameters["location"]["name"])
-		self.location.append(self.parameters["location"]["country"])
-		self.location.append(self.parameters["location"]["tz_id"])
+		self.data["current"].append(self.json_response["current"]["temp_c"])
+		self.data["current"].append(self.json_response["forecast"]["forecastday"][0]["day"]["condition"]["text"])
+		self.data["location"].append(self.json_response["location"]["name"])
+		self.data["location"].append(self.json_response["location"]["country"])
+		self.data["location"].append(self.json_response["location"]["tz_id"])
     	
 		for i in range(self.amount_of_days):
-			self.min_temp.append(self.parameters["forecast"]["forecastday"][i]["day"]["mintemp_c"])
-			self.max_temp.append(self.parameters["forecast"]["forecastday"][i]["day"]["maxtemp_c"])
-			self.avg_temp.append(self.parameters["forecast"]["forecastday"][i]["day"]["avgtemp_c"])
-			self.humidity.append(self.parameters["forecast"]["forecastday"][i]["day"]["avghumidity"])
-			self.icon.append(self.parameters["forecast"]["forecastday"][i]["day"]["condition"]["icon"])
+			self.data["min_temp"].append(self.json_response["forecast"]["forecastday"][i]["day"]["mintemp_c"])
+			self.data["max_temp"].append(self.json_response["forecast"]["forecastday"][i]["day"]["maxtemp_c"])
+			self.data["avg_temp"].append(self.json_response["forecast"]["forecastday"][i]["day"]["avgtemp_c"])
+			self.data["humidity"].append(self.json_response["forecast"]["forecastday"][i]["day"]["avghumidity"])
+			self.data["icon"].append(self.json_response["forecast"]["forecastday"][i]["day"]["condition"]["icon"])
 
 
 	def string_format_weekdays(self) -> None:
@@ -63,14 +63,14 @@ class GeoData():
 		current_date = date.today()
 
 		for i in range(self.amount_of_days):
-			self.days.append(current_date + timedelta(i))
+			self.data["days"].append(current_date + timedelta(i))
 
 		for i in range(self.amount_of_days):
-			self.days[i] = self.days[i].strftime("%A")
+			self.data["days"][i] = self.data["days"][i].strftime("%A")
 
-		self.days[0] = "Today"
+		self.data["days"][0] = "Today"
 		try:
-			self.days[1] = "Tomorrow"
+			self.data["days"][1] = "Tomorrow"
 
 		finally:
 			return
@@ -94,17 +94,17 @@ class GeoData():
 		with open("background/sunny.txt", "r") as sunny:
 			sunny_image = sunny.read()
 
-		state = self.parameters["forecast"]["forecastday"][0]["day"]["condition"]["text"]
+		state = self.json_response["forecast"]["forecastday"][0]["day"]["condition"]["text"]
 	
 		if "rain" in state:
-			self.image = rainy_image
+			self.data["image"] = rainy_image
 		
 		elif "snow" in state:
-			self.image = snow_image
+			self.data["image"] = snow_image
 
 		elif "Cloudy" in state or "Overcast" in state:
-			self.image = cloudy_image
+			self.data["image"] = cloudy_image
 
 		else:
-			self.image = sunny_image
+			self.data["image"] = sunny_image
 	
